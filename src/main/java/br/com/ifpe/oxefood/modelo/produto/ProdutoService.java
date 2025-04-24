9cef8197dd7549649f511964fd8054dde055176b
Aluno <ifpe@aluno>
2025-04-24 20:53:26 -0300

    Classe Produto

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -0,0 +1,23 @@
+package br.com.ifpe.oxefood.modelo.produto;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service //o que torna ela um service é a notação @Service ou @Controler
+public class ProdutoService {
+
+   @Autowired //Instacia um objeto em tempo de execução, não precisa fazer o new na mão, é mais rapido
+   private ProdutoRepository repository;
+
+   @Transactional //ou ele roda td em relação ao banco ou não faz nada, se uma delas falhar as outras são desfeitas
+   //Recebe um objeto e repassa para o repositorio 
+   public Produto save(Produto produto) {
+
+       cliente.setHabilitado(Boolean.TRUE);
+       return repository.save(cliente);//cadastra um registo no banco e retorna um objeto que foi cadastrado
+   }
+
+}
+
